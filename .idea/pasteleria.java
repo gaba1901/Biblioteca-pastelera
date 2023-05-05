26fd4ba13d7b77522bd46c257f3d16f3ce9cb0a0
David Guaba <75450596+gaba1901@users.noreply.github.com>
2023-05-04 23:36:39 -0500

    Primera interfaz

---
+++ .idea/pasteleria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package guaba.pasteleria;
+
+/**
+ *
+ * @author guaba
+ */
+public class Pasteleria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
