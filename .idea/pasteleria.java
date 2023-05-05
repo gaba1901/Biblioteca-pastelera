a208d6ba4281435fdde1d06971b6fca853d57489
David Guaba <75450596+gaba1901@users.noreply.github.com>
2023-05-05 08:31:49 -0500

    Primer vistazo de la interfaz

--- .idea/pasteleria.java
+++ .idea/pasteleria.java
@@ -12,5 +12,11 @@ public class Pasteleria {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // Crear una instancia del JFrame que deseas llamar
+        Principal Principal = new Principal();
+
+        // Mostrar el JFrame
+        Principal.setVisible(true);
+
     }
 }
